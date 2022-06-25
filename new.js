becb41c79780e148a7a7eaf807deae6c870da4e1
ABIOLA TADE <83308307+teecrown@users.noreply.github.com>
2022-06-25 14:14:10 +0100

    Add files via upload

---
+++ new.js
@@ -0,0 +1,12 @@
+alert  ('My first Javascript');
+function Name(params) {}
+   Name.displayName= "Tade Abiola" 
+console.log(Name.displayName);
+
+function height(params) {}
+height=  "1.76"
+console.log(height);
+
+function country() {}
+country=  "Nigeria"
+console.log(country);
